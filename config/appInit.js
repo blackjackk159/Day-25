c9863e31fbe386865e52e892a55ccc81f0f8e0b3
blackjackk159 <blackjackk159@gmail.com>
2023-03-23 15:14:31 +0800

    fix: route post to user

--- config/appInit.js
+++ config/appInit.js
@@ -1,8 +1,8 @@
 require("./dbInit");
 const app = require("express")();
-const postRoutes = require("../routes/post");
+const userRoutes = require("../routes/user");
 const errorHandler = require("../utils/errorHandler");
-app.use("/posts", postRoutes);
+app.use("/users", userRoutes);
 
 // error handler
 // 錯誤處理的 middleware 相較一般 middleware 會多一個 err 引數
